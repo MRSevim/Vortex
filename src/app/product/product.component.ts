6c2033a9bf90714849b62c94c0d99414b91b1b98
MRSevim <lionrasit@gmail.com>
2023-12-14 21:44:07 +0300

    fixed route param subs

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -22,11 +22,13 @@ export class ProductComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    const routeParams = this.route.snapshot.paramMap;
-    const productIdFromRoute = Number(routeParams.get('productId'));
-    this.product = this.productsService.getProductById(
-      productIdFromRoute
-    ) as ProductInterface;
-    this.productStarsImg = `../assets/stars-images/${this.product.stars}-stars.jpg`;
+    this.route.params.subscribe((params) => {
+      const routeParams = this.route.snapshot.paramMap;
+      const productIdFromRoute = Number(routeParams.get('productId'));
+      this.product = this.productsService.getProductById(
+        productIdFromRoute
+      ) as ProductInterface;
+      this.productStarsImg = `../assets/stars-images/${this.product.stars}-stars.jpg`;
+    });
   }
 }
